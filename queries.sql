f4b53ba8ad4c9179bc701d37245e582548033281
biehnstalk1912 <biehnstalk1912@gmail.com>
2025-08-26 22:18:25 -0500

    init

--- queries.sql
+++ queries.sql
@@ -1,2 +1,6 @@
-SELECT player_id
-FROM common_player_info
+SELECT g.game_id, g.game_date, p.play_id, p.event_type, p.player_id
+FROM game_info g
+JOIN play_by_play p
+  ON g.game_id = p.game_id
+WHERE g.game_date > '2010-01-01'
+LIMIT 100
