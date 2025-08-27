4928351e8cf775bf14becd3067f9869028d45bc7
biehnstalk1912 <biehnstalk1912@gmail.com>
2025-08-26 23:10:31 -0500

    3

--- NBA.py
+++ NBA.py
@@ -1,4 +1,5 @@
 import pandas as pd
+
 player_index = pd.read_csv("player_index.csv")
 player_scoring_rs = pd.read_csv("player_stats_traditional_rs.csv")
 player_scoring_post = pd.read_csv("player_stats_traditional_po.csv")
@@ -10,3 +11,4 @@ print(df.columns)
 selected_columns = ["player_id", "player_name_po", "team_id_po", "team_id_rs", "age_po", "fg_pct_rs", "fg_pct_po", "fg3_pct_po", "fg3_pct_rs", "pts_po", "pts_rs", "plus_minus_rs", "plus_minus_po", "season"]
 df = df[selected_columns]
 print(df.head(10))
+
