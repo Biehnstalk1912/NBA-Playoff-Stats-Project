46641dcb1492f4331fc2adfa3d785dfab2d3a404
biehnstalk1912 <biehnstalk1912@gmail.com>
2025-08-26 23:10:13 -0500

    3

--- NBA.py
+++ NBA.py
@@ -2,12 +2,11 @@ import pandas as pd
 player_index = pd.read_csv("player_index.csv")
 player_scoring_rs = pd.read_csv("player_stats_traditional_rs.csv")
 player_scoring_post = pd.read_csv("player_stats_traditional_po.csv")
-print(player_scoring_rs.head(5))
-print(player_scoring_post.head(5))
-
-df = pd.merge(player_scoring_post, player_scoring_rs, on="PLAYER_ID", how="right", suffixes=("_po", "_rs"))
+df = pd.merge(player_scoring_post, player_scoring_rs, on=["PLAYER_ID", "SEASON"], how="left", suffixes=("_po", "_rs"))
 
 df.columns = df.columns.str.lower()
 
 print(df.columns)
-selected_columns = ["player_id", "player_name_po", "age"]
+selected_columns = ["player_id", "player_name_po", "team_id_po", "team_id_rs", "age_po", "fg_pct_rs", "fg_pct_po", "fg3_pct_po", "fg3_pct_rs", "pts_po", "pts_rs", "plus_minus_rs", "plus_minus_po", "season"]
+df = df[selected_columns]
+print(df.head(10))
